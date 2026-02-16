a717f66f32a6aa57b32765db36d7c99b8f14f3f3
CWIR3 <humum.shaikh@outlook.com>
2026-02-16 23:22:04 +0530

    Create timer.cpp

---
+++ STM32F4_BlackPill/Projects/Timer/timer.cpp
@@ -0,0 +1,49 @@
+unsigned long currentMillis;
+unsigned long currentMillis2;
+
+unsigned long previousMillis;
+unsigned long previousMillis2;
+
+const long interval = 1000;
+const long interval2 = 2000;
+
+int redLed;
+int greenLed;
+
+void setup(){
+  previousMillis=0;
+  previousMillis2=0;
+  pinMode(PB1,OUTPUT);
+  pinMode(PA2,OUTPUT);
+}
+
+void loop(){
+
+  currentMillis = millis();
+  currentMillis2 = millis();
+
+  if(currentMillis-previousMillis>=interval){
+
+    previousMillis = currentMillis;
+
+    if(redLed==LOW){
+      redLed = HIGH;
+    }else{
+      redLed=LOW;
+    }
+  }
+
+  if(currentMillis2-previousMillis2>=interval2){
+
+    previousMillis2=currentMillis2;
+
+    if(greenLed==LOW){
+      greenLed=HIGH;
+    }else{
+      greenLed=LOW;
+    }
+  }
+
+  digitalWrite(PB1,redLed);
+  digitalWrite(PA2,greenLed);
+}
