735b0da58648552dbe45b3ea4eed80c64f76430b
CWIR3 <humum.shaikh@outlook.com>
2026-02-22 22:51:58 +0530

    Create analogRead.cpp

---
+++ STM32F4_BlackPill/Projects/Analog_Read/analogRead.cpp
@@ -0,0 +1,15 @@
+int sensePin = PA0;
+
+void setup(){
+  while(Serial.available()==0){
+
+  }
+  Serial.begin(115200);
+  Serial.println("Initialized");
+  pinMode(sensePin,INPUT);
+}
+
+void loop(){
+  Serial.println(analogRead(sensePin));
+  delay(300);
+}
