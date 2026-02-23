71949a7ec874e2292a6adc91eea7a757078b500a
CWIR3 <humum.shaikh@outlook.com>
2026-02-23 23:19:53 +0530

    Add serial read control functionality

---
+++ STM32F4_BlackPill/Projects/Serial_Read_Control/serialRead.cpp
@@ -0,0 +1,25 @@
+int code;
+
+void setup(){
+  Serial.begin(115200);
+  while(Serial.available()==0){
+  
+  }
+  Serial.println("Initialized");
+  pinMode(PB1,OUTPUT);
+}
+
+void loop(){
+
+  while(Serial.available()==0){
+
+  }
+  code = Serial.parseInt();
+
+  if(code==69){
+    digitalWrite(PB1,HIGH);
+  }
+  if(code==66){
+    digitalWrite(PB1,LOW);
+  }
+}
