b9c86d0cd2dc72da98c1e97017f3b6cf51ad27e2
CWIR3 <humum.shaikh@outlook.com>
2026-02-27 00:23:08 +0530

    Update counter_main.cpp

--- STM32F4_BlackPill/Projects/Counter/counter_main.cpp
+++ STM32F4_BlackPill/Projects/Counter/counter_main.cpp
@@ -1,1 +1,37 @@
+#include<Adafruit_SSD1306.h>
 
+int x;
+
+#define OLED_RESET -1
+
+Adafruit_SSD1306 oled(OLED_RESET);
+
+void setup(){
+  Serial.begin(115200);
+  oled.begin(SSD1306_SWITCHCAPVCC,0x3C);
+  oled.clearDisplay();
+}
+
+void loop(){
+  x = 0;
+  for(x=0;x<=100;x=x+1){
+  oled.setTextColor(WHITE);
+  oled.setTextSize(2);
+  oled.setCursor(56,10);
+  oled.println(x);
+  oled.display();
+  delay(100);
+  oled.clearDisplay();
+  }
+  x = 100;
+  for(x=100;x>=1;x=x-1){
+      oled.setTextColor(WHITE);
+  oled.setTextSize(2);
+  oled.setCursor(56,10);
+  oled.println(x);
+  oled.display();
+  delay(100);
+  oled.clearDisplay();
+  }  
+ 
+}
